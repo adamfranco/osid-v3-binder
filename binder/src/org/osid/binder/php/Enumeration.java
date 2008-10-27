932d4f54ac471f3d5b12c0f65f7ec8edcf1a7553
Adam Franco <afranco@middlebury.edu>
2008-10-27 19:09:48 -0400

    Enumeration now generates a PHP representation.
    Not sure if this is the best way to represent this use
    of 'enum' in PHP. Will need further investigation when
    more is known about the intended use.

--- binder/src/org/osid/binder/php/Enumeration.java
+++ binder/src/org/osid/binder/php/Enumeration.java
@@ -72,22 +72,24 @@ public class Enumeration
 
 
     void print(PrintStream out) {
-	out.println("//");
-	out.println("// " + getName());
-	out.println("//");
-	out.println("//     Specifies the OSID definition for " + getName() + ".");
-	out.println("//");
-	getCopyright().printPlain(out, "// ");
+	out.println("/**");
+	out.println(" * " + getClassName(getName()));
+	out.println(" * ");
+	out.println(" *     Specifies the OSID definition for " + getClassName(getName()) + ".");
+	out.println(" * ");
+	getCopyright().printPlain(out, " * ");
 	out.println();
-	out.println("//");
-	getLicense().printPlain(out, "//     ");
+	out.println(" * ");
+	getLicense().printPlain(out, " *     ");
 	out.println();
-	out.println("//");
+	out.println(" * ");
+	out.println(" * @package " + getPackageName(getName()));
+	out.println(" */");
 	out.println();
 
-	out.println("package " + getPackageName(getName()) + ";");
-	out.println();
-	out.println();
+// 	out.println("package " + getPackageName(getName()) + ";");
+// 	out.println();
+// 	out.println();
 
 	out.println("/**");
 	if (getDescription() != null) {
@@ -98,114 +100,123 @@ public class Enumeration
 	out.println(" */");
         out.println();
 
-	out.println("public enum " + getClassName(getName()) + " {");
+// 	out.println("trigger_error('Binding of enums not complete. " + getClassName(getName()) + " not bound');");
+	
+	out.println("class " + getClassName(getName()) + " {");
 	out.println();
 
-	boolean first = true;
+// 	boolean first = true;
 	for (org.osid.binder.EnumerationItem item: getItems()) {
-	    if (!first) {
-		out.println(",");
-		out.println();
-	    } else {
-		first = false;
-	    }
+// 	    if (!first) {
+// 		out.println(",");
+// 		out.println();
+// 	    } else {
+// 		first = false;
+// 	    }
 
 	    out.print("    /** ");
 	    item.getDescription().printHtml(out, "        ", 10);
 	    out.println("*/");
-	    out.print("    " + item.getBinderName());
+	    out.print("    static $" + item.getBinderName());
+	     if (getName().equals("osid.OSID")) {
+	out.println(";");
+	     } else {
+	     	 out.println(" = \"" + getServiceName(item.getBinderName()) + "\";");
+	     }
+   		out.println();
+	}
 
-	    if (getName().equals("osid.OSID")) {
-		out.print("(\"" + getServiceName(item.getBinderName()) + 
-			  "\", \"org.osid." +
-			  getServiceName(item.getBinderName()) + 
-			  "\", \"" +
-			  getManagerName(item.getBinderName()) + 
-			  "\", \"" +
-			  getProxyManagerName(item.getBinderName()) +
-			  "\", \"" + 
-			  item.getDescription().getText() +
-			  "\")");
-	    } else {
-		out.print("(\"" + item.getBinderName().toLowerCase() + 
-			  "\", \"" +
-			  item.getDescription().getText() + 
-			  "\")");
+// 	out.println(");");
+
+	out.println();
+	
+	out.println("    public static function values() {");
+	out.println("        $ret = array();");
+	out.println("        $ref = new ReflectionClass(__CLASS__);");
+	out.println("        $properties = $ref->getProperties();");
+	out.println("        foreach ($properties as $property)");
+	out.println("            $ret[$property->getName()] = $property->getValue();");
+	out.println("        return $ret;");
+	out.println("    }");
+	
+	out.println("}");
+	
+	if (getName().equals("osid.OSID")) {
+		out.println();
+		out.println();
+		out.println("class OsidEnumItem {");
+		out.println("    private $service;");
+		out.println("    private $osid;");
+		out.println("    private $manager;");
+		out.println("    private $proxyManager;");
+		out.println("    private $description;");
+		out.println();
+		out.println("    public function __construct($service, $osid, $manager, $proxyManager, $description) {");
+		out.println("        $this->service = $service;");
+		out.println("        $this->osid = $osid;");
+		out.println("        $this->manager = $manager;");
+		out.println("        $this->proxyManager = $proxyManager;");
+		out.println("        $this->description = $description;");
+		out.println("    }");
+		out.println();
+		out.println("    public function getOSIDServiceName() {");
+		out.println("        return $this->service;");
+		out.println("    }");
+		out.println();
+		out.println("    public function getOSIDPackageName() {");
+		out.println("        return $this->osid;");
+		out.println("    }");
+		out.println();
+		out.println("    public function getManager() {");
+		out.println("        return $this->manager;");
+		out.println("    }");
+		out.println();
+		out.println("    public function getProxyManager() {");
+		out.println("        return $this->proxyManager;");
+		out.println("    }");
+		out.println();
+		out.println("    public function getDescription() {");
+		out.println("        return $this->description;");
+		out.println("    }");
+		out.println("}");
+	}
+	
+	out.println();
+	out.println();
+	for (org.osid.binder.EnumerationItem item: getItems()) {
+		if (getName().equals("osid.OSID")) {
+			out.println();
+			out.print("/** ");
+			item.getDescription().printHtml(out, "        ", 10);
+			out.println("*/");
+			out.print(getClassName(getName()) + "::$" + item.getBinderName() + " = ");
+	
+		   
+			out.print("new OsidEnumItem ");
+			out.println("("
+				  + "\"" + getServiceName(item.getBinderName()) + "\", " 
+				  + "null, " 
+				  + "\"" + getManagerName(item.getBinderName()) + "\", " 
+				  + "\"" + getProxyManagerName(item.getBinderName()) + "\", "
+				  + "\"" + item.getDescription().getText() + "\"" 
+				  + ");");
 	    }
 	}
-
-	out.println(";");
-
-	if (getName().equals("osid.OSID")) {
-	    out.println();
-	    out.println();
-	    out.println("    private final String service;");
-	    out.println("    private final String osid;");
-	    out.println("    private final String manager;");
-	    out.println("    private final String proxyManager;");
-	    out.println("    private final String description;");
-	    out.println();
-	    out.println("    OSID(String service, String osid, String manager, String proxyManager, String description) {");
-	    out.println("        this.service      = service;");
-	    out.println("        this.osid         = osid;");
-	    out.println("        this.manager      = manager;");
-	    out.println("        this.proxyManager = proxyManager;");
-	    out.println("        this.description  = description;");
-	    out.println("    }");
-	    out.println();
-	    out.println("    public String getOSIDServiceName() {");
-	    out.println("        return (this.service);");
-	    out.println("    }");
-	    out.println();
-	    out.println("    public String getOSIDPackageName() {");
-	    out.println("        return (this.osid);");
-	    out.println("    }");
-	    out.println();
-	    out.println("    public String getManager() {");
-	    out.println("        return (this.manager);");
-	    out.println("    }");
-	    out.println();
-	    out.println("    public String getProxyManager() {");
-	    out.println("        return (this.proxyManager);");
-	    out.println("    }");
-	    out.println();
-	    out.println("    public String getDescription() {");
-	    out.println("        return (this.description);");
-	    out.println("    }");
-	} else {
-	    out.println();
-	    out.println();
-	    out.println("    private final String displayName;");
-	    out.println("    private final String description;");
-	    out.println();
-	    out.println("    " + getClassName(getName()) + "(String displayName, String description) {");
-	    out.println("        this.displayName = displayName;");
-	    out.println("        this.description = description;");
-	    out.println("    }");
-	    out.println();
-	    out.println("    public String getDisplayName() {");
-	    out.println("        return (this.displayName);");
-	    out.println("    }");
-	    out.println();
-	    out.println("    public String getDescription() {");
-	    out.println("        return (this.description);");
-	    out.println("    }");
-	}
-
-	out.println("}");
 	return;
     }
 
 
     private String getClassName(final String path) {
-	int pos = path.lastIndexOf(".");
-	return (path.substring(pos + 1));
+	return (org.osid.binder.php.Interface.getClassName(path));
+// 	int pos = path.lastIndexOf(".");
+// 	return (path.substring(pos + 1));
     }
 
 
     private String getPackageName(final String path) {
-	int pos = path.lastIndexOf(".");
-	return ("org." + path.substring(0, pos));
+	return (org.osid.binder.php.Interface.getPackageName(path));
+// 	int pos = path.lastIndexOf(".");
+// 	return ("org." + path.substring(0, pos));
     }
 
 
@@ -215,14 +226,19 @@ public class Enumeration
 
 
     private String getManagerName(final String osid) {
-	return ("org.osid." + osid.toLowerCase() + "." +
+    return ("osid_" + osid.toLowerCase() + "_" +
 		capitalize(osid.toLowerCase()) + "Manager");
+// 	return ("org.osid." + osid.toLowerCase() + "." +
+// 		capitalize(osid.toLowerCase()) + "Manager");
     }
 
 
     private String getProxyManagerName(final String osid) {
-	return ("org.osid." + osid.toLowerCase() + "." + 
+    return ("osid_" + osid.toLowerCase() + "_" +
 		capitalize(osid.toLowerCase()) + "ProxyManager");
+
+// 	return ("org.osid." + osid.toLowerCase() + "." + 
+// 		capitalize(osid.toLowerCase()) + "ProxyManager");
     }
 
     
